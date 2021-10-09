33fa64a602f1c3acf4aafb9a3490baf5614989f1
Ahmed Raza <raza1778@gmail.com>
2021-10-09 13:11:49 +0500

    Translate service injection and styles method

--- src/Mapbox.php
+++ src/Mapbox.php
@@ -2,6 +2,7 @@
 
 namespace Drupal\mapbox;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\StringTranslation\TranslationManager;
 
 /**
  * Class Mapbox.
@@ -16,14 +17,35 @@ class Mapbox {
   protected $configFactory;
 
   /**
+   * Drupal\Core\StringTranslation\TranslationManager definition.
+   *
+   * @var \Drupal\Core\StringTranslation\TranslationManager
+   */
+  protected $stringTranslation;
+
+  /**
    * Constructs a new Mapbox object.
    */
-  public function __construct(ConfigFactoryInterface $config_factory) {
-    $this->configFactory = $config_factory;
+  public function __construct(ConfigFactoryInterface $config_factory, TranslationManager $string_translation) {
+    $this->configFactory      = $config_factory;
+    $this->stringTranslation  = $string_translation;
   }
 
   public function getStyle() {
-    //
+    return 'mapbox://styles/mapbox/streets-v11';
+  }
+
+  public function getStyles() {
+    return [
+      'mapbox://styles/mapbox/streets-v11'            => $this->stringTranslation->translate('Mapbox Streets'),
+      'mapbox://styles/mapbox/outdoors-v11'           => $this->stringTranslation->translate('Mapbox Outdoors'),
+      'mapbox://styles/mapbox/light-v10'              => $this->stringTranslation->translate('Mapbox Light'),
+      'mapbox://styles/mapbox/dark-v10'               => $this->stringTranslation->translate('Mapbox Dark'),
+      'mapbox://styles/mapbox/satellite-v9'           => $this->stringTranslation->translate('Mapbox Satellite'),
+      'mapbox://styles/mapbox/satellite-streets-v11'  => $this->stringTranslation->translate('Mapbox Satellite Streets'),
+      'mapbox://styles/mapbox/navigation-day-v1'      => $this->stringTranslation->translate('Mapbox Navigation Day'),
+      'mapbox://styles/mapbox/navigation-night-v1'    => $this->stringTranslation->translate('Mapbox Navigation Night'),
+    ];
   }
 
 }
